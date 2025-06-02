96ba4be1dfb3f5a07836aa663cfcde684b824c0c
Saif Ullah <saifullahl5210@gmail.com>
2025-06-02 18:45:36 +0000

    fixed email

--- email.js
+++ email.js
@@ -4,7 +4,7 @@ function showHideEmail() {
         document.getElementById('email').innerHTML = "Show my email";
         shown = false;
     } else {
-        var myemail = "<a href='mailto:ullahs3" + "@" + "udayton.edu'>ullahs3" + "@" + "udayton.edu</a>";
+        var myemail = "<a href='mailto:saifullahl5210" + "@" + "gmail.com'>saifullahl5210" + "@" + "gmail.com</a>";
         document.getElementById('email').innerHTML = myemail;
         shown = true;
     }
